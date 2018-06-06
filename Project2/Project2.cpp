d2a19bc61e9b999aeb19e1576740b505598dfe3a
kbpositive <kadbaldwin@gmail.com>
2018-06-06 10:04:09 -0500

    Created target variable; Added description of output.

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -6,15 +6,16 @@ using namespace std;
 
 int length = 0;
 int sum = 0;
+int target = 4000000;
 
 int fibonacci(int num);
 
 int main(){
-  while(fibonacci(length) <= 4000000){
+  while(fibonacci(length) <= target){
     sum += fibonacci(length);
     length += 3;
   }
-    cout << sum;
+    cout << "The sum of all even valued terms in the Fibonacci sequence between " << target << " and 0 is: " << sum;
 }
 
 int fibonacci(int num){
