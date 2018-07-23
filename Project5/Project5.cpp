0b3affa17925f837f193d71815a633c856abda4d
kbpositive <kadbaldwin@gmail.com>
2018-07-22 22:01:13 -0500

    Finished general solution to problem 5.

--- Project5/Project5.cpp
+++ Project5/Project5.cpp
@@ -1,29 +1,29 @@
+// Project Euler 5
 #include<iostream>
 #include<cmath>
-#include<numeric>
 #include<vector>
+#include<numeric>
 
 using namespace std;
 
-vector<int> spi = {2}; // Smallest Positive Integer
-int minfact = 1; // lowest desired factor for spi
-int maxfact = 20; // highest desired factor for spi
+int minfact = 2; // lowest divisor for spi
+vector<int> spi = {2}; // smallest positive integer
+int maxfact = 20; // highest divisor for spi
 
 int main(){
-  cout << "The smallest positive integer able to be divided by every integer from " << minfact << " to " << maxfact << " is ";
-  for(int count = minfact; count <= maxfact; ++count){
-    minfact = count;
+  cout << "The smallest positive integer that is evenly divisible by every integer from " << minfact-1 << " to " << maxfact << " is ";
+  int count = 2;
+  while(count <= maxfact){
+  minfact = count;
     for(int factor = 0; factor < spi.size(); ++factor){
       if(minfact % spi[factor] == 0){
         minfact /= spi[factor];
-      }
-      if(minfact == 1){
-        break;
       }
     }
     if(minfact > 1){
       spi.push_back(minfact);
     }
+    ++count;
   }
   cout << accumulate(spi.begin(),spi.end(),1,multiplies<int>()) << ".\n";
 }
