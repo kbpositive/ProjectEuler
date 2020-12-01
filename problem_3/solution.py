185308c3e0fa4c48db51ce76dfc2d9cd018f26d8
Kadeem Baldwin <kadbaldwin@gmail.com>
2020-11-30 19:09:37 -0600

    Completed solution for problem 3,

--- problem_3/solution.py
+++ problem_3/solution.py
@@ -1,5 +1,41 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 
-# What is the largest prime topFactor of the number 600851475143?
+# What is the largest prime factor of the number 600851475143?
 
-import typing
+from math import sqrt, ceil
+from typing import List
+
+def strToListInt(str: str) -> List[int]:
+    return [int(digit) for digit in str]
+
+def listDivide(numerator: List[int], denominator: int) -> (List[int], bool):
+    remainder = 0
+    quotient = numerator[:]
+    for index, digit in enumerate(quotient):
+        fullDigit = digit + remainder*10
+        quotient[index] = fullDigit // denominator
+        remainder = fullDigit % denominator
+    return quotient, bool(remainder)
+
+def primeFactors(num: str) -> List[int]:
+    listNum = strToListInt(num)
+    if len(listNum) % 2:
+        squareRoot = ceil(sqrt(listNum[0]+1))*10**((len(listNum)-1)//2)
+    else:
+        squareRoot = ceil(sqrt(listNum[0]*10+listNum[1]+1))*10**((len(listNum)-2)//2)
+
+    primes = []
+    remainderExists = True
+    factor = 2
+    while factor < squareRoot:
+        remainderExists = listDivide(listNum, factor)[1]
+        if remainderExists is not True:
+            primes.append(factor)
+            while remainderExists is not True:
+                listNum = listDivide(listNum, factor)[0]
+        factor += 1
+    primes.append(int(''.join([str(digit) for digit in listNum])))
+    return primes
+
+pfs = primeFactors("600851475143")
+print(max(pfs))
